0a7b3114702d51af8bad8ba3a2df5341baf5fd25
HarumaIto <itoharm@gmail.com>
2026-03-16 22:48:39 +0900

    add: gameFlagsを登録

--- src/scenarios/main.ts
+++ src/scenarios/main.ts
@@ -1,7 +1,9 @@
 import { Story } from "narraleaf-react";
 import { prologueScene } from "./prologue";
+import { gameFlags } from "../store/gameState";
 
 // 全シーンはプロローグから jumpTo で連鎖しているため、
 // entry にプロローグを設定するだけでストーリー全体が動く
 export const mainStory = new Story("main");
+mainStory.registerPersistent(gameFlags);
 mainStory.entry(prologueScene);
