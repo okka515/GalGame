0091fa029b016cc77e46ee651a1d986a2edd796f
Ayato Okada <ayato@AyatonoMacBook-Air-5.local>
2026-03-23 01:27:01 +0900

    fix: 音声ファイルのパスから /public プレフィックスを削除

--- src/store/gameBgm.ts
+++ src/store/gameBgm.ts
@@ -1,43 +1,43 @@
 import { Sound } from "narraleaf-react";
 
 export const massuBaseBgm = new Sound({
-  src: "/public/audio/bgm/massu_base_bgm.mp3",
+  src: "/audio/bgm/massu_base_bgm.mp3",
   loop: true,
   volume: 0.3,
 });
 
 export const tonappiBaseBgm = new Sound({
-  src: "/public/audio/bgm/tonappi_base_bgm.mp3",
+  src: "/audio/bgm/tonappi_base_bgm.mp3",
   loop: true,
   volume: 0.3,
 });
 
 export const saasanBaseBgm = new Sound({
-  src: "/public/audio/bgm/saasan_base_bgm.mp3",
+  src: "/audio/bgm/saasan_base_bgm.mp3",
   loop: true,
   volume: 0.3,
 });
 
 export const haruchiroBaseBgm = new Sound({
-  src: "/public/audio/bgm/haruchiro_base_bgm.mp3",
+  src: "/audio/bgm/haruchiro_base_bgm.mp3",
   loop: true,
   volume: 0.3,
 });
 
 export const packBaseBgm = new Sound({
-  src: "/public/audio/bgm/pack_base_bgm.mp3",
+  src: "/audio/bgm/pack_base_bgm.mp3",
   loop: true,
   volume: 0.3,
 });
 
 export const packHentekoBgm = new Sound({
-  src: "/public/audio/bgm/pack_henteko_bgm.mp3",
+  src: "/audio/bgm/pack_henteko_bgm.mp3",
   loop: true,
   volume: 0.3,
 });
 
 export const haruchiroLoveBgm = new Sound({
-  src: "/public/audio/bgm/haruchiro_love_bgm.mp3",
+  src: "/audio/bgm/haruchiro_love_bgm.mp3",
   loop: true,
   volume: 0.3,
 })
