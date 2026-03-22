7d3344305e72010931819677ee9f5b4c77950f7b
HarumaIto <itoharm@gmail.com>
2026-03-22 16:59:33 +0900

    add: 透過画像に変更し、喋るタイミングで立ち絵が表示されるように変更

--- src/characters/index.ts
+++ src/characters/index.ts
@@ -7,24 +7,24 @@ export const yuujin = new Character(null);
 export const pack = new Character("ぱっく", {
   color: "#f59e0b",
 });
-export const packImg = new Image({ src: "/characters/pack.jpg" });
+export const packImg = new Image({ src: "/characters/pack.png" });
 
 export const massu = new Character("まっすー", {
   color: "#ec4899",
 });
-export const massuImg = new Image({ src: "/characters/massu.jpg" });
+export const massuImg = new Image({ src: "/characters/massu.png" });
 
 export const saasan = new Character("さーさん", {
   color: "#10b981",
 });
-export const saasanImg = new Image({ src: "/characters/saasan.JPG" });
+export const saasanImg = new Image({ src: "/characters/saasan.png" });
 
 export const haruchiro = new Character("はるちろ", {
   color: "#6366f1",
 });
-export const haruchiroImg = new Image({ src: "/characters/harutiro.jpg" });
+export const haruchiroImg = new Image({ src: "/characters/haruchiro.png" });
 
 export const tonapi = new Character("となっぴー", {
   color: "#06b6d4",
 });
-export const tonapiImg = new Image({ src: "/characters/tonappi.jpg" });
+export const tonapiImg = new Image({ src: "/characters/tonappi.png" });
